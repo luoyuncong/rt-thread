297ceed479e4b01a4e654ccb58509952813b1832
luoyuncong <luoyuncong@126.com>
2017-09-01 16:08:05 +0800

    Update ringbuffer.c
    修正环形缓冲put_force操作中，某些情况下，整个缓冲区被清空的bug.

--- components/drivers/src/ringbuffer.c
+++ components/drivers/src/ringbuffer.c
@@ -107,7 +107,7 @@ rt_size_t rt_ringbuffer_put_force(struct rt_ringbuffer *rb,
     space_length = rt_ringbuffer_space_len(rb);
 
     if (length > space_length)
-        length = rb->buffer_size;
+        length = rb->buffer_size < length ? rb->buffer_size : length;
 
     if (rb->buffer_size - rb->write_index > length)
     {
